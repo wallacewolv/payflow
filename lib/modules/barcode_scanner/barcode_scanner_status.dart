24ab3138560006c12b2fc73d3fab281bf0b3eaaa
Wallace <wallace_wolv@hotmail.com>
2021-07-03 20:27:34 -0300

    Created BarcodeScannerStatus

---
+++ lib/modules/barcode_scanner/barcode_scanner_status.dart
@@ -0,0 +1,16 @@
+class BarcodeScannerStatus {
+  final bool isCameraAvailable;
+  final String error;
+  final String barcode;
+  BarcodeScannerStatus({
+    required this.isCameraAvailable,
+    this.error = "",
+    this.barcode = "",
+  });
+
+  bool get showCamera => isCameraAvailable && error.isEmpty;
+
+  bool get hasError => error.isNotEmpty;
+
+  bool get hasBarcode => barcode.isNotEmpty;
+}
